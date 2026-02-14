51e6423ec282b6dd3091de7bf5646d3e1e560940
jlbuild <juliabuildbot@gmail.com>
2026-02-14 19:45:03 +0000

    Xorg_libXdamage_jll build 1.1.7+0

--- src/Xorg_libXdamage_jll.jl
+++ src/Xorg_libXdamage_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Xorg_libXdamage")
-JLLWrappers.@generate_main_file("Xorg_libXdamage", UUID("0aeada51-83db-5f97-b67e-184615cfc6f6"))
+JLLWrappers.@generate_main_file("Xorg_libXdamage", Base.UUID("0aeada51-83db-5f97-b67e-184615cfc6f6"))
 end  # module Xorg_libXdamage_jll
